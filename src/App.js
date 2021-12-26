1de84fe0ff7eaabde32d2d72073122c55558387a
jonesvrajan <jonesvrajan@gmail.com>
2021-12-26 19:51:53 +0530

    Project Demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+const apiCall = {
+  "type": "subscribe",
+    "payload": {
+        "channels": [{
+                "name": "v2/ticker",
+                "symbols": [
+                    "USDT",
+                    "BTCUSD",
+                    "BTCUSDT",
+                    "BNB",
+                    "ADABTC"
+            ]},
+        ]
+    }
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //const [mark, setMark ] = useState([]);
+    const [markOne, setMarkOne ] = useState([]);
+    const [tableData, setTableData ] = useState([]);
+    //const [symbols, setSymbols ] = useState([]);
+    const [loader, setLoader ] = useState(true);
+    //const inputSymbols = apiCall.payload.channels[0].symbols;
+    
+    const fetchData = async () => {
+        const response = await fetch("https://api.delta.exchange/v2/products");
+        const data = await response.json();
+        setTableData(data.result);
+        setLoader(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+
+        const ws = new WebSocket('wss://socket.delta.exchange');
+        
+        ws.onopen = (event) => {
+            ws.send(JSON.stringify(apiCall));
+        };
+        ws.onmessage = function (event) {
+            const json = JSON.parse(event.data);
+
+            try {
+                if ((json.type === 'subscriptions')) {
+                    console.log("Loading")
+                }
+
+                else {
+                    //const sym = json.symbol;
+                    const price = json.mark_price;
+                    setMarkOne(markOne => [...markOne.slice(0,2), price]);
+                    //setSymbols(symbols => [...symbols.slice(0,2), sym])
+     
+                }
+
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        //clean up function
+        return () => ws.close();
+    }, []);
+
+    const markPrice = (i) => {
+        
+        if (i.symbol === 'C-MATIC-3.1-281221') {
+            return markOne[0]
+        }
+        return markOne[2];
+    }
+    
+    const tableDataC = tableData.map((item, index) => (
+         
+        <div className="table" key={index}>
+            <p> {item.symbol}</p>
+            <p> {item.description}</p>
+            <p>{item.underlying_asset.name}</p>   
+            <p>{markPrice(item)}</p>
+        </div>
+    ));
+
+    return (
+        
+        <div className="page">
+            <header><img src={logo} alt="Delta Exchange"/></header>
+            <div className="container">
+                <div className="table-header table">
+                    <div><h3>Symbol</h3></div>
+                    <div><h3>Description</h3></div>
+                    <div><h3>Underlying Asset</h3></div>
+                    <div><h3>Mark Price</h3></div>
+                </div>
+                {loader? <p className="loading">Loading</p>: <div>{tableDataC}</div>}
+                
+            </div>
+        </div>
+    );
 }
 
 export default App;
